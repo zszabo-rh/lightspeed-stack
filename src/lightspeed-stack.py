22f15940206b6df8bc41eb9c355583b00af8cf41
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-06 09:34:00 +0200

    Configuration loader

--- src/lightspeed-stack.py
+++ src/lightspeed-stack.py
@@ -1,8 +1,20 @@
 """Lightspeed stack."""
 
+import yaml
+
 from runners.uvicorn import start_uvicorn
-import version
+from models.config import Configuration
+
+
+def load_configuration(filename: str) -> Configuration:
+    """Load configuration from YAML file."""
+    with open(filename, encoding="utf-8") as fin:
+        config_dict = yaml.safe_load(fin)
+        return Configuration(**config_dict)
+
 
 if __name__ == "__main__":
     print("Lightspeed stack")
+    configuration = load_configuration("lightspeed-stack.yaml")
+    print(configuration)
     start_uvicorn()
