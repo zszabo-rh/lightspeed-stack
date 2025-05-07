cedd5dc177acc45d15c841472f179e9fdd5f6036
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-07 11:11:00 +0200

    Query route

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from app import routers
 import version
 
--- src/app/routers.py
+++ src/app/routers.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 
-from app.endpoints import info, models, root
+from app.endpoints import info, models, root, query
 
 
 def include_routers(app: FastAPI) -> None:
@@ -14,3 +14,4 @@ def include_routers(app: FastAPI) -> None:
     app.include_router(root.router)
     app.include_router(info.router, prefix="/v1")
     app.include_router(models.router, prefix="/v1")
+    app.include_router(query.router, prefix="/v1")
