c33d48e403d0c59bf4959c62f7f62d263ec41cb7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-07 11:11:57 +0200

    Model for model list

--- src/app/endpoints/models.py
+++ src/app/endpoints/models.py
@@ -7,12 +7,41 @@ from typing import Any, Optional
 from fastapi import APIRouter, Request
 from llama_stack_client import Agent, AgentEventLogger, RAGDocument, LlamaStackClient
 
+from models.responses import ModelsResponse
+
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=["models"])
 
 
-@router.get("/models")
-def models_endpoint_handler(request: Request) -> list[dict]:
+models_responses: dict[int | str, dict[str, Any]] = {
+    200: {
+        "models": [
+            {
+                "identifier": "all-MiniLM-L6-v2",
+                "metadata": {"embedding_dimension": 384},
+                "api_model_type": "embedding",
+                "provider_id": "ollama",
+                "provider_resource_id": "all-minilm:latest",
+                "type": "model",
+                "model_type": "embedding",
+            },
+            {
+                "identifier": "llama3.2:3b-instruct-fp16",
+                "metadata": {},
+                "api_model_type": "llm",
+                "provider_id": "ollama",
+                "provider_resource_id": "llama3.2:3b-instruct-fp16",
+                "type": "model",
+                "model_type": "llm",
+            },
+        ]
+    },
+}
+
+
+@router.get("/models", responses=models_responses)
+def models_endpoint_handler(request: Request) -> ModelsResponse:
     client = LlamaStackClient(base_url="http://localhost:8321")
     models = client.models.list()
-    return [dict(m) for m in models]
+    m = [dict(m) for m in models]
+    return ModelsResponse(models=m)
--- src/models/config.py
+++ src/models/config.py
@@ -1,7 +1,16 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+
+class LLamaStackConfiguration(BaseModel):
+    """Llama stack configuration."""
+
+    url: str
 
 
 class Configuration(BaseModel):
     """Global service configuration."""
 
     name: str
+    llama_stack: LLamaStackConfiguration
