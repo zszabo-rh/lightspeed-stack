0b5e683257dc9b2fd31ffc8b47407c3b6b4e82e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-07 11:12:18 +0200

    Query response endpoint

---
+++ src/app/endpoints/query.py
@@ -0,0 +1,25 @@
+"""Handler for REST API call to provide answer to query."""
+
+import logging
+from typing import Any
+
+from fastapi import APIRouter, Request
+
+from version import __version__
+from models.responses import QueryResponse
+
+logger = logging.getLogger(__name__)
+router = APIRouter(tags=["models"])
+
+
+query_response: dict[int | str, dict[str, Any]] = {
+    200: {
+        "query": "User query",
+        "answer": "LLM ansert",
+    },
+}
+
+
+@router.get("/query", responses=query_response)
+def info_endpoint_handler(request: Request) -> QueryResponse:
+    return QueryResponse(query="foo", response="bar")
