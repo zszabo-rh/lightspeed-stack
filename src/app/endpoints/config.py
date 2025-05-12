73e01d3f6ba82a822b91efecb3d349df1b50a02a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-12 09:33:19 +0200

    Config endpoint

---
+++ src/app/endpoints/config.py
@@ -0,0 +1,26 @@
+"""Handler for REST API call to configuration."""
+
+import logging
+from typing import Any
+
+from fastapi import APIRouter, Request
+
+from version import __version__
+from models.config import Configuration
+from configuration import configuration
+
+logger = logging.getLogger(__name__)
+router = APIRouter(tags=["config"])
+
+
+get_config_responses: dict[int | str, dict[str, Any]] = {
+    200: {
+        "name": "foo bar baz",
+        "llama_stack": {"url": "http://localhost:8321", "api_key": "xyzzy"},
+    },
+}
+
+
+@router.get("/config", responses=get_config_responses)
+def config_endpoint_handler(request: Request) -> Configuration:
+    return configuration.configuration
