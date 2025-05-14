7b06e187d297d98d7a41d08b7eb75b3be715200a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-14 08:27:12 +0200

    Configuration: llama stack as library client

--- src/models/config.py
+++ src/models/config.py
@@ -1,13 +1,30 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 from typing import Optional
+from typing_extensions import Self
 
 
 class LLamaStackConfiguration(BaseModel):
     """Llama stack configuration."""
 
-    url: str
+    url: Optional[str] = None
     api_key: Optional[str] = None
+    use_as_library_client: Optional[bool] = None
+
+    @model_validator(mode="after")
+    def check_llama_stack_model(self) -> Self:
+        if self.url is None:
+            if self.use_as_library_client is None:
+                raise ValueError(
+                    "LLama stack URL is not specified and library client mode is not specified"
+                )
+            if self.use_as_library_client is False:
+                raise ValueError(
+                    "LLama stack URL is not specified and library client mode is not enabled"
+                )
+        if self.use_as_library_client is None:
+            self.use_as_library_client = False
+        return self
 
 
 class Configuration(BaseModel):
