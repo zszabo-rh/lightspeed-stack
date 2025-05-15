658d2af318813788b7f104d73fa983e072fbd2b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-15 08:46:24 +0200

    Logging improvement

--- src/lightspeed-stack.py
+++ src/lightspeed-stack.py
@@ -9,11 +9,9 @@ from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)  # set level=20 or logging.INFO to turn off debug
-logger = logging.getLogger("rich")
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
