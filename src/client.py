574dfbef859a2f4e602274458e9c0e58260306ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-22 08:39:39 +0200

    [minor] Check if library_path is specified

--- src/client.py
+++ src/client.py
@@ -13,12 +13,16 @@ def get_llama_stack_client(
     llama_stack_config: LLamaStackConfiguration,
 ) -> LlamaStackClient:
     if llama_stack_config.use_as_library_client is True:
-        logger.info("Using Llama stack as library client")
-        client = LlamaStackAsLibraryClient(
-            llama_stack_config.library_client_config_path
-        )
-        client.initialize()
-        return client
+        if llama_stack_config.library_client_config_path is not None:
+            logger.info("Using Llama stack as library client")
+            client = LlamaStackAsLibraryClient(
+                llama_stack_config.library_client_config_path
+            )
+            client.initialize()
+            return client
+        msg = "Configuration problem: library_client_config_path option is not set"
+        logger.error(msg)
+        raise Exception(msg)
     else:
         logger.info("Using Llama stack running as a service")
         return LlamaStackClient(
