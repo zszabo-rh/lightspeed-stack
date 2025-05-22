a8de42779ce416a5a7890ecc706455a8ed300aed
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-22 08:40:01 +0200

    Updated unit tests accordingly

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,6 +1,7 @@
 """Unit tests for functions defined in src/client.py."""
 
 import os
+import pytest
 from unittest.mock import patch
 
 from client import get_llama_stack_client
@@ -29,3 +30,19 @@ def test_get_llama_stack_remote_client():
     )
     client = get_llama_stack_client(cfg)
     assert client is not None
+
+
+@patch.dict(os.environ, {"INFERENCE_MODEL": "llama3.2:3b-instruct-fp16"})
+def test_get_llama_stack_wrong_configuration():
+    cfg = LLamaStackConfiguration(
+        url=None,
+        api_key=None,
+        use_as_library_client=True,
+        library_client_config_path="ollama",
+    )
+    cfg.library_client_config_path = None
+    with pytest.raises(
+        Exception,
+        match="Configuration problem: library_client_config_path option is not set",
+    ):
+        get_llama_stack_client(cfg)
