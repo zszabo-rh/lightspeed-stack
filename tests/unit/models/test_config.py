012edc3b996395e33caeb9ec36d0a131ed8449bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-26 10:51:08 +0200

    Updated unit tests accordingly

--- tests/unit/models/test_config.py
+++ tests/unit/models/test_config.py
@@ -2,7 +2,35 @@
 
 import pytest
 
-from models.config import Configuration, LLamaStackConfiguration
+from models.config import Configuration, LLamaStackConfiguration, ServiceConfiguration
+
+
+def test_service_configuration_constructor() -> None:
+    """Test the ServiceConfiguration constructor."""
+    s = ServiceConfiguration()
+    assert s is not None
+
+    assert s.host == "localhost"
+    assert s.port == 8080
+    assert s.auth_enabled is False
+    assert s.workers == 1
+    assert s.color_log is True
+    assert s.access_log is True
+
+
+def test_service_configuration_port_value() -> None:
+    """Test the ServiceConfiguration port value validation."""
+    with pytest.raises(ValueError, match="Port value should not be negative"):
+        ServiceConfiguration(port=-1)
+
+    with pytest.raises(ValueError, match="Port value should be less than 65536"):
+        ServiceConfiguration(port=100000)
+
+
+def test_service_configuration_workers_value() -> None:
+    """Test the ServiceConfiguration workers value validation."""
+    with pytest.raises(ValueError, match="Workers must be set to at least 1"):
+        ServiceConfiguration(workers=-1)
 
 
 def test_llama_stack_configuration_constructor() -> None:
