2179a3befb019e12ba4fc441e4d71665c44ad9ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-08 22:42:11 +0200

    Configuration for runner

--- tests/unit/runners/test_uvicorn_runner.py
+++ tests/unit/runners/test_uvicorn_runner.py
@@ -3,14 +3,17 @@
 from unittest.mock import patch
 
 
+from models.config import ServiceConfiguration
 from runners.uvicorn import start_uvicorn
 
 
 def test_start_uvicorn() -> None:
     """Test the function to start Uvicorn server."""
+    configuration = ServiceConfiguration(host="localhost", port=8080, workers=1)
+
     # don't start real Uvicorn server
     with patch("uvicorn.run") as mocked_run:
-        start_uvicorn()
+        start_uvicorn(configuration)
         mocked_run.assert_called_once_with(
             "app.main:app",
             host="localhost",
