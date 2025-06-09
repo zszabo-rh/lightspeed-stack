ff7ef9b121cbabb18ac1761edd8550e0baf42494
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-09 14:07:26 +0200

    Unit test for /info endpoint handler

---
+++ tests/unit/app/endpoints/test_info.py
@@ -0,0 +1,32 @@
+from app.endpoints.info import info_endpoint_handler
+from configuration import AppConfig
+
+
+def test_info_endpoint(mocker):
+    """Test the info endpoint handler."""
+    config_dict = {
+        "name": "foo",
+        "service": {
+            "host": "localhost",
+            "port": 8080,
+            "auth_enabled": False,
+            "workers": 1,
+            "color_log": True,
+            "access_log": True,
+        },
+        "llama_stack": {
+            "api_key": "xyzzy",
+            "url": "http://x.y.com:1234",
+            "use_as_library_client": False,
+        },
+        "user_data_collection": {
+            "feedback_disabled": True,
+        },
+    }
+    cfg = AppConfig()
+    cfg.init_from_dict(config_dict)
+    request = None
+    response = info_endpoint_handler(request)
+    assert response is not None
+    assert response.name is not None
+    assert response.version is not None
