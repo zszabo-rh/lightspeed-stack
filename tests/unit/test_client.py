2635296a81a2eebde1d41af577b1e9747012c9b7
John Boos <45039134+jrobertboos@users.noreply.github.com>
2025-06-09 16:21:41 -0400

    LCORE-173: Fixed Unit Tests (#83)
    * Fixed Unit Test
    
    * Added Sqlite (Required to pass unit tests)
    
    * Removed Environment Varaibles
    
    * Fixed Ruff

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,45 +1,41 @@
 """Unit tests for functions defined in src/client.py."""
 
-import os
 import pytest
-from unittest.mock import patch
 
 from client import get_llama_stack_client
 from models.config import LLamaStackConfiguration
 
 
 # [tisnik] Need to resolve dependencies on CI to be able to run this tests
-@patch.dict(os.environ, {"INFERENCE_MODEL": "llama3.2:3b-instruct-fp16"})
-def _test_get_llama_stack_library_client() -> None:
+def test_get_llama_stack_library_client() -> None:
     cfg = LLamaStackConfiguration(
         url=None,
         api_key=None,
         use_as_library_client=True,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
+
     client = get_llama_stack_client(cfg)
     assert client is not None
 
 
-@patch.dict(os.environ, {"INFERENCE_MODEL": "llama3.2:3b-instruct-fp16"})
 def test_get_llama_stack_remote_client() -> None:
     cfg = LLamaStackConfiguration(
         url="http://localhost:8321",
         api_key=None,
         use_as_library_client=False,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
     client = get_llama_stack_client(cfg)
     assert client is not None
 
 
-@patch.dict(os.environ, {"INFERENCE_MODEL": "llama3.2:3b-instruct-fp16"})
 def test_get_llama_stack_wrong_configuration() -> None:
     cfg = LLamaStackConfiguration(
         url=None,
         api_key=None,
         use_as_library_client=True,
-        library_client_config_path="ollama",
+        library_client_config_path="./tests/configuration/minimal-stack.yaml",
     )
     cfg.library_client_config_path = None
     with pytest.raises(
