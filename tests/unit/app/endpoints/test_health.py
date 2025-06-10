d55a618bff250be9368386c72d901a13c2b72b40
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-10 14:53:48 +0200

    Unit test for /liveness REST API endpoint

--- tests/unit/app/endpoints/test_health.py
+++ tests/unit/app/endpoints/test_health.py
@@ -1,4 +1,4 @@
-from app.endpoints.health import readiness_probe_get_method
+from app.endpoints.health import readiness_probe_get_method, liveness_probe_get_method
 
 
 def test_readiness_probe(mocker):
@@ -7,3 +7,10 @@ def test_readiness_probe(mocker):
     assert response is not None
     assert response.ready is True
     assert response.reason == "service is ready"
+
+
+def test_liveness_probe(mocker):
+    """Test the liveness endpoint handler."""
+    response = liveness_probe_get_method()
+    assert response is not None
+    assert response.alive is True
