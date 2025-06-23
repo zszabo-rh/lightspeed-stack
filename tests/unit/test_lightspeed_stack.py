c31464f6d2b6756b44228b6f0a64872e8f468dcb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-23 10:07:03 +0200

    Simple unit test for argparser

---
+++ tests/unit/test_lightspeed_stack.py
@@ -0,0 +1,10 @@
+"""Unit tests for functions defined in src/lightspeed_stack.py."""
+
+from lightspeed_stack import create_argument_parser
+
+
+def test_create_argument_parser():
+    """Test for create_argument_parser function."""
+    arg_parser = create_argument_parser()
+    # nothing more to test w/o actual parsing is done
+    assert arg_parser is not None
