4c454662a37aa662b69a2a2bd6df0f0cd5086d8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-23 09:51:40 +0200

    LCORE-199: more unit tests for Uvicorn runner

--- tests/unit/runners/test_uvicorn_runner.py
+++ tests/unit/runners/test_uvicorn_runner.py
@@ -2,8 +2,10 @@
 
 from unittest.mock import patch
 
+from pydantic import FilePath
 
-from models.config import ServiceConfiguration
+
+from models.config import ServiceConfiguration, TLSConfiguration
 from runners.uvicorn import start_uvicorn
 
 
@@ -23,6 +25,79 @@ def test_start_uvicorn() -> None:
             ssl_certfile=None,
             ssl_keyfile=None,
             ssl_keyfile_password="",
+            use_colors=True,
+            access_log=True,
+        )
+
+
+def test_start_uvicorn_different_host_port() -> None:
+    """Test the function to start Uvicorn server using custom configuration."""
+    configuration = ServiceConfiguration(host="x.y.com", port=1234, workers=10)
+
+    # don't start real Uvicorn server
+    with patch("uvicorn.run") as mocked_run:
+        start_uvicorn(configuration)
+        mocked_run.assert_called_once_with(
+            "app.main:app",
+            host="x.y.com",
+            port=1234,
+            workers=10,
+            log_level=20,
+            ssl_certfile=None,
+            ssl_keyfile=None,
+            ssl_keyfile_password="",
+            use_colors=True,
+            access_log=True,
+        )
+
+
+def test_start_uvicorn_empty_tls_configuration() -> None:
+    """Test the function to start Uvicorn server using empty TLS configuration."""
+    tls_config = TLSConfiguration()
+    configuration = ServiceConfiguration(
+        host="x.y.com", port=1234, workers=10, tls_config=tls_config
+    )
+
+    # don't start real Uvicorn server
+    with patch("uvicorn.run") as mocked_run:
+        start_uvicorn(configuration)
+        mocked_run.assert_called_once_with(
+            "app.main:app",
+            host="x.y.com",
+            port=1234,
+            workers=10,
+            log_level=20,
+            ssl_certfile=None,
+            ssl_keyfile=None,
+            ssl_keyfile_password="",
+            use_colors=True,
+            access_log=True,
+        )
+
+
+def test_start_uvicorn_tls_configuration() -> None:
+    """Test the function to start Uvicorn server using custom TLS configuration."""
+    tls_config = TLSConfiguration(
+        tls_certificate_path="tests/configuration/server.crt",
+        tls_key_path="tests/configuration/server.key",
+        tls_key_password="tests/configuration/password",
+    )
+    configuration = ServiceConfiguration(
+        host="x.y.com", port=1234, workers=10, tls_config=tls_config
+    )
+
+    # don't start real Uvicorn server
+    with patch("uvicorn.run") as mocked_run:
+        start_uvicorn(configuration)
+        mocked_run.assert_called_once_with(
+            "app.main:app",
+            host="x.y.com",
+            port=1234,
+            workers=10,
+            log_level=20,
+            ssl_certfile=FilePath("tests/configuration/server.crt"),
+            ssl_keyfile=FilePath("tests/configuration/server.key"),
+            ssl_keyfile_password="tests/configuration/password",
             use_colors=True,
             access_log=True,
         )
