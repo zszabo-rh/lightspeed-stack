5b4e4850b5ef49465f3b877942817e090e1ca1a3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-25 09:51:13 +0200

    Script to generate documentation for all modules

---
+++ scripts/gen_doc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+
+import ast
+from pathlib import Path
+
+
+for path in Path("src").rglob("*"):
+    if path.is_dir():
+        directory = path
+        cwd = os.getcwd()
+        os.chdir(directory)
+        print(directory)
+
+        try:
+            with open("README.md", "w", encoding="utf-8", newline="\n") as indexfile:
+                print(f"# List of source files stored in `{directory}` directory", file=indexfile)
+                print("", file=indexfile)
+                files = sorted(os.listdir())
+
+                for file in files:
+                    if file.endswith(".py"):
+                        print(f"## [{file}]({file})", file=indexfile)
+                        with open(file, "r", encoding="utf-8") as fin:
+                            source = fin.read()
+                        try:
+                            mod = ast.parse(source)
+                            doc = ast.get_docstring(mod)
+                        except SyntaxError:
+                            doc = None
+                        if doc:
+                            print(doc.splitlines()[0], file=indexfile)
+                        print(file=indexfile)
+        finally:
+            os.chdir(cwd)
