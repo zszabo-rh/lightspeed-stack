383f0b214ec6d776b9d9241b25dce11d65100c6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-07 09:49:51 +0200

    Added type hints

--- tests/unit/utils/test_types.py
+++ tests/unit/utils/test_types.py
@@ -8,30 +8,30 @@ from utils.types import GraniteToolParser
 class TestGraniteToolParser:
     """Unit tests for functions defined in utils/types.py."""
 
-    def test_get_tool_parser_when_model_is_is_not_granite(self):
+    def test_get_tool_parser_when_model_is_is_not_granite(self) -> None:
         """Test that the tool_parser is None when model_id is not a granite model."""
         assert (
             GraniteToolParser.get_parser("ollama3.3") is None
         ), "tool_parser should be None"
 
-    def test_get_tool_parser_when_model_id_does_not_start_with_granite(self):
+    def test_get_tool_parser_when_model_id_does_not_start_with_granite(self) -> None:
         """Test that the tool_parser is None when model_id does not start with granite."""
         assert (
             GraniteToolParser.get_parser("a-fine-trained-granite-model") is None
         ), "tool_parser should be None"
 
-    def test_get_tool_parser_when_model_id_starts_with_granite(self):
+    def test_get_tool_parser_when_model_id_starts_with_granite(self) -> None:
         """Test that the tool_parser is not None when model_id starts with granite."""
         tool_parser = GraniteToolParser.get_parser("granite-3.3-8b-instruct")
         assert tool_parser is not None, "tool_parser should not be None"
 
-    def test_get_tool_calls_from_completion_message_when_none(self):
+    def test_get_tool_calls_from_completion_message_when_none(self) -> None:
         """Test that get_tool_calls returns an empty array when CompletionMessage is None."""
         tool_parser = GraniteToolParser.get_parser("granite-3.3-8b-instruct")
         assert tool_parser is not None, "tool parser was not returned"
         assert tool_parser.get_tool_calls(None) == [], "get_tool_calls should return []"
 
-    def test_get_tool_calls_from_completion_message_when_not_none(self):
+    def test_get_tool_calls_from_completion_message_when_not_none(self) -> None:
         """Test that get_tool_calls returns an empty array when CompletionMessage has no tool_calls."""  # pylint: disable=line-too-long
         tool_parser = GraniteToolParser.get_parser("granite-3.3-8b-instruct")
         assert tool_parser is not None, "tool parser was not returned"
@@ -41,7 +41,9 @@ class TestGraniteToolParser:
             completion_message
         ), "get_tool_calls should return []"
 
-    def test_get_tool_calls_from_completion_message_when_message_has_tool_calls(self):
+    def test_get_tool_calls_from_completion_message_when_message_has_tool_calls(
+        self,
+    ) -> None:
         """Test that get_tool_calls returns the tool_calls when CompletionMessage has tool_calls."""
         tool_parser = GraniteToolParser.get_parser("granite-3.3-8b-instruct")
         assert tool_parser is not None, "tool parser was not returned"
