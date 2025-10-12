ee3680fa1312d931f353788d127e19366d6dcfe2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-12 13:23:06 +0200

    Added type hints into test_lightspeed_stack.py

--- tests/unit/test_lightspeed_stack.py
+++ tests/unit/test_lightspeed_stack.py
@@ -3,7 +3,7 @@
 from lightspeed_stack import create_argument_parser
 
 
-def test_create_argument_parser():
+def test_create_argument_parser() -> None:
     """Test for create_argument_parser function."""
     arg_parser = create_argument_parser()
     # nothing more to test w/o actual parsing is done
