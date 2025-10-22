ff5b82684eda2d30b412e362b542425acd93027e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-22 14:09:49 +0200

    Unit tests for ClusterQuotaLimiter

---
+++ tests/unit/quota/test_cluster_quota_limiter.py
@@ -0,0 +1,175 @@
+"""Unit tests for ClusterQuotaLimiter class."""
+
+import datetime
+
+import pytest
+from pytest_mock import MockerFixture
+
+from models.config import (
+    QuotaLimiterConfiguration,
+    PostgreSQLDatabaseConfiguration,
+    SQLiteDatabaseConfiguration,
+    QuotaHandlersConfiguration,
+)
+from quota.cluster_quota_limiter import ClusterQuotaLimiter
+from quota.quota_exceed_error import QuotaExceedError
+
+
+def create_quota_limiter(
+    name: str, initial_quota: int, quota_limit: int
+) -> ClusterQuotaLimiter:
+    """Create new quota limiter instance."""
+    configuration = QuotaHandlersConfiguration()
+    configuration.sqlite = SQLiteDatabaseConfiguration(
+        db_path=":memory:",
+    )
+    configuration.limiters = [
+        QuotaLimiterConfiguration(
+            type="cluster_limiter",
+            name="foo",
+            initial_quota=initial_quota,
+            quota_increase=1,
+            period="5 days",
+        ),
+    ]
+    quota_limiter = ClusterQuotaLimiter(configuration, initial_quota, 1)
+    assert quota_limiter is not None
+    return quota_limiter
+
+
+def test_connected():
+    """Test the connected method."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+    assert quota_limiter.connected()
+
+
+def test_init_quota():
+    """Test the init quota operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    assert (
+        str(quota_limiter) == "ClusterQuotaLimiter: initial quota: 1000 increase by: 1"
+    )
+
+
+def test_available_quota():
+    """Test the available quota operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota
+
+
+def test_consume_tokens():
+    """Test the consume tokens operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota
+
+    quota_limiter.consume_tokens(0, 1, "foo")
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 1
+
+    quota_limiter.consume_tokens(1, 0, "foo")
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 2
+
+    quota_limiter.consume_tokens(1, 1, "foo")
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 4
+
+
+def test_increase_quota():
+    """Test the increase_quota operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota
+
+    quota_limiter.consume_tokens(1, 1, "foo")
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 2
+
+    quota_limiter.increase_quota("foo")
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 1
+
+
+def test_ensure_available_quota():
+    """Test the ensure_available_quota operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    quota_limiter.ensure_available_quota("foo")
+
+
+def test_ensure_available_quota_no_quota():
+    """Test the ensure_available_quota operation."""
+    initial_quota = 0
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    with pytest.raises(QuotaExceedError, match="Cluster has no available tokens"):
+        quota_limiter.ensure_available_quota("foo")
+
+
+def test_revoke_quota():
+    """Test the revoke_quota operation."""
+    initial_quota = 1000
+    quota_limit = 100
+
+    quota_limiter = create_quota_limiter("foo", initial_quota, quota_limit)
+
+    # init quota for given cluster
+    quota_limiter._init_quota()
+
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota
+
+    quota_limiter.consume_tokens(1, 1, "foo")
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota - 2
+
+    quota_limiter.revoke_quota("foo")
+    available_quota = quota_limiter.available_quota("foo")
+    assert available_quota == initial_quota
